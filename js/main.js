2263ba3cfa61237bea3a321135bbd2c4ea27f76a
dantudor <dtudor01@gmail.com>
2019-06-10 20:41:48 +0100

    Fixed head on load as well

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function ($) {
 
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+    $('#header').addClass('header-fixed');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+    $('#header').removeClass('header-fixed');
+  }
+
   // Header fixed and Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
